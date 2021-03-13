3ba17089603a109c6a84ba606731661397dc02e9
Bryan Lee <bryan.lee@lynxcreations.com>
2021-03-13 16:50:16 -0500

    features btn bug fix, changed menu overlay JS, from display to visibility

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById('menu-overlay').style.display = 'initial';
+  document.getElementById('menu-overlay').style.visibility = 'initial';
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-  document.getElementById('menu-overlay').style.display = 'none';
+  document.getElementById('menu-overlay').style.visibility = 'hidden';
 }
